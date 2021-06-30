e482a00956c5c6dd0c59b618118943aa55e5e3be
alanwcg <alancintra7@gmail.com>
2021-06-30 19:21:41 -0300

    fix

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -42,7 +42,7 @@ app.use(Sentry.Handlers.tracingHandler());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
-app.use('/avatar', express.static(`${upload.tmpFolder}/cars`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
